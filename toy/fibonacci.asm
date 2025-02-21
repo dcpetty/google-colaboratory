ca5cefe959581b4cb510e49dba23960b9250fcbe
DCPetty <github@patton-petty.net>
2025-02-21 17:29:25 -0500

    Updated Fibonacci algorithm, including comments.

--- toy/fibonacci.asm
+++ toy/fibonacci.asm
@@ -2,20 +2,20 @@
 #
 # Fibonacci program, loaded at location 0x40
 #
-GOTO START          # TOY simulator requires an instruction at 0x10
+GOTO START              # TOY simulator requires instruction at 0x10
 : PC 40
 : START
-R[1] <- 01
-R[9] <- M[ COUNT ]  # forward reference to COUNT
-R[A] <- 01
-R[B] <- 01
+R[1] <- 01              # R1 <- decrement
+R[9] <- M[ COUNT ]      # R9 <- COUNT (forward reference)
+R[A] <- 00              # RA <- previous Fibonacci number
+R[B] <- 01              # RB <- current Fibonacci number
 WHILE<>0 R[9]
-    WRITE R[A]
-    R[C] <- R[A] + R[B]
-    R[A] <- R[B]
-    R[B] <- R[C]
-    R[9] <- R[9] - R[1]
+    WRITE R[B]          # send current Fibonacci number to stdout
+    R[C] <- R[A] + R[B] # calculate next Fibonacci number
+    R[A] <- R[B]        # shift previous Fibonacci number
+    R[B] <- R[C]        # shift current Fibonacci number
+    R[9] <- R[9] - R[1] # decrement counter
 REPEAT
-: STOP              # the HALT statement
-HALT
+: STOP
+HALT                    # the HALT statement
 . COUNT 0018
